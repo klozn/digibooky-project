21485f84c8ea6683a14b32feb8f6d32b6daf233c
Klaas Devits <klaas.devits@gmail.com>
2021-10-20 14:57:41 +0200

    initial commit

---
+++ src/main/java/com/vitsebeirenvantmaeskantje/digibookyproject/DigibookyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vitsebeirenvantmaeskantje.digibookyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigibookyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigibookyProjectApplication.class, args);
+    }
+
+}
